98675a4648a90bec5ad4d832ffdacd4699400966
Masterious <83734987+mipcit1010@users.noreply.github.com>
2022-07-25 21:40:26 -0400

    Update cluster.js

--- cluster.js
+++ cluster.js
@@ -1,9 +1,10 @@
+const { red, green, blue, magenta, cyan, white, gray, black } = require("chalk");
 const Cluster = require('discord-hybrid-sharding');
 const { Manager } = require("discord-hybrid-sharding");
-const totalShards = "auto";
-const config = require('./config.json');
+const totalShards = 4;
 const colors = require("colors");
-const shardsPerCluster = 1;
+const shardsPerCluster = 2;
+const config = require(`./config.json`)
 const manager = new Cluster.Manager(`./index.js`, {
     totalShards: totalShards, // Use 'auto' if u want it to be Auto.
     shardsPerClusters: shardsPerCluster, 
@@ -11,10 +12,12 @@ const manager = new Cluster.Manager(`./index.js`, {
     token: process.env.token || config.token,
     respawn: true, 
     usev13: true,
-}); // Check pings
-// no, before you hadjust the top stuff & it worked fine
+});
+
+
 manager.on("clusterCreate", cluster => {
-    console.log(`[SHARDING-MANAGER]: `.magenta + `Launched Cluster #${cluster.id} | ${cluster.id+1}/${cluster.manager.totalClusters} [${cluster.manager.shardsPerClusters}/${cluster.manager.totalShards} Shards]`.green)
+    
+  console.log(red(`[👍] :`)+`: Launched Cluster #${cluster.id} & ${cluster.id+1}/${cluster.manager.totalClusters} [${cluster.manager.shardsPerClusters}/${cluster.manager.totalShards} Shards]`.yellow)
 
     cluster.on("death", function () {
         console.log(`${colors.red.bold(`Cluster ${cluster.id} died..`)}`);
@@ -28,25 +31,25 @@ manager.on("clusterCreate", cluster => {
         }
     })
     cluster.on("error", e => {
-        console.log(`${colors.red.bold(`Cluster ${cluster.id} errored ..`)}`);
+      console.log(red(`[❌] :`)+`: Cluster #${cluster.id} ERROR`.red.bold)
         console.error(e);
     })
     
     cluster.on("disconnect", function () {
-        console.log(`${colors.red.bold(`Cluster ${cluster.id} disconnected..`)}`);
+        console.log(red(`[❌] :`)+`: Cluster #${cluster.id} DISCONNECTED`.red.bold)
     });
 
     cluster.on("reconnecting", function () {
-        console.log(`${colors.yellow.bold(`Cluster ${cluster.id} reconnecting..`)}`);
+        console.log(red(`[❌] :`)+`: Cluster #${cluster.id} RECONNECTING`.red.bold)
     });
 
     cluster.on("close", function (code) {
-        console.log(`${colors.red.bold(`Cluster ${cluster.id} close with code ${code}`)}`);
+        console.log(red(`[❌] :`)+`: Cluster #${cluster.id} CLOSED`.red.bold)
     });
 
     cluster.on("exit", function (code) {
-        console.log(`${colors.red.bold(`Cluster ${cluster.id} exited with code ${code}`)}`);
+        console.log(red(`[❌] :`)+`: Cluster #${cluster.id} EXITED`.red.bold)
     });
 });
-manager.on("debug", (d) => d.includes("[CM => Manager] [Spawning Clusters]") ? console.log(d) : "")
-manager.spawn({timeout: -1}).catch((e)=>console.log(e))
+manager.on("debug", (d) => d.includes("Cluster Manager (LIST):") ? console.log(d) : "")
+manager.spawn({timeout: -1});
