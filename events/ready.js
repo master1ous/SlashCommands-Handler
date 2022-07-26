17676d1f4cbdb6fe919ee4607f6d98218af5b243
Masterious <83734987+mipcit1010@users.noreply.github.com>
2022-07-25 21:41:13 -0400

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,9 +2,9 @@ const { red, green, blue, magenta, cyan, white, gray, black } = require("chalk")
 const client = require("../index");
 
 client.on("ready", () => {
-    console.log(cyan.bold(`🪐 https://discord.azury.live`));
-    console.log(green(`[🚩BOT] → ` + magenta(`${client.user.tag}`) +  ` is up & ready!`));
-    console.log(green(`[🚩BOT] → https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot%20applications.commands`))
+    console.log(cyan.bold(``+blue(`[⚡] :`)+`: Powered by: https://discord.gg/azury`));
+    console.log(green(``+blue(`[🤖] :`)+`: Logged in as: ` + magenta(`${client.user.tag}`)));
+    console.log(green(``+blue(`[🔗] :`)+`: https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot%20applications.commands`))
     client.user.setActivity(client.config.activity.replace("{shards}", client.cluster.id)
     , { type: client.config.status.type })
     client.user.setStatus(client.config.status)
